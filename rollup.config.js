f842c0162e3905b3e5f69d24978804f6e7eb5737
Nico Rehwaldt <git_nikku@nixis.de>
2018-04-01 11:51:30 +0200

    chore(package): add browser field

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
+import pkg from './package.json';
+
 function pgl(plugins=[]) {
   return [
     nodeResolve({
@@ -23,27 +25,12 @@ function pgl(plugins=[]) {
 
 
 export default [
-  {
-    input: 'lib/index.js',
-    output: {
-      file: 'dist/index.mjs',
-      format: 'es'
-    },
-    plugins: pgl()
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      file: 'dist/index.js',
-      format: 'cjs'
-    },
-    plugins: pgl()
-  },
+  // browser-friendly UMD build
   {
     input: 'lib/index.js',
     output: {
       name: 'MinDom',
-      file: 'dist/min-dom.js',
+      file: pkg.browser,
       format: 'umd'
     },
     plugins: pgl()
@@ -52,11 +39,19 @@ export default [
     input: 'lib/index.js',
     output: {
       name: 'MinDom',
-      file: 'dist/min-dom.min.js',
+      file: pkg.browser.replace(/\.js$/, '.min.js'),
       format: 'umd'
     },
     plugins: pgl([
       uglify()
     ])
+  },
+  {
+    input: 'lib/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: pgl()
   }
 ];
