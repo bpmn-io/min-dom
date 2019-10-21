17ae9d7aace41c4be341aa91094ab3115f72d31e
Markus Mårtensson <markus@mayam.com>
2019-10-21 12:37:09 +0200

    Have closest depend on matches-selector 1.2.0 rather than an older version for which Shadow DOM might cause TypeError issues.

--- test/closest.js
+++ test/closest.js
@@ -22,4 +22,22 @@ describe('closest', function() {
     expect(closest(node, '.root', true)).to.equal(node);
   });
 
+  it('should work with document fragments and shadow dom', function() {
+
+    var node = domify('<div class="root">' +
+                        '<div class="parent">' +
+                          '<div class="child"></div>' +
+                        '</div>' +
+                      '</div>');
+    var child = query('.child', node);
+
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(node);
+
+    // when
+    expect(closest(child, '.root')).to.equal(node);
+    expect(closest(node, '.root', true)).to.equal(node);
+    expect(closest(node, '.outside')).to.be.undefined;
+  });
+
 });
