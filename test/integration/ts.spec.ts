b4ff7e2bd823597cc0676f654316a761dbab3df4
Martin Stamm <martin.stamm@camunda.com>
2022-05-18 11:23:28 +0200

    test: verify types can be consumed

---
+++ test/integration/ts.spec.ts
@@ -0,0 +1,23 @@
+import * as MinDom from '../../lib';
+
+import { expect } from 'chai';
+
+
+describe('typed', function() {
+
+  it('should expose utilities', function() {
+    expect(MinDom.assignStyle).to.exist;
+    expect(MinDom.attr).to.exist;
+    expect(MinDom.classes).to.exist;
+    expect(MinDom.clear).to.exist;
+    expect(MinDom.closest).to.exist;
+    expect(MinDom.delegate).to.exist;
+    expect(MinDom.domify).to.exist;
+    expect(MinDom.event).to.exist;
+    expect(MinDom.matches).to.exist;
+    expect(MinDom.query).to.exist;
+    expect(MinDom.queryAll).to.exist;
+    expect(MinDom.remove).to.exist;
+  });
+
+});
