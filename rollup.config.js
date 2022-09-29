64f8859460c9ceb9c65ce1ad3f26288c9e2f810c
Nico Rehwaldt <git_nikku@nixis.de>
2022-09-29 17:33:28 +0200

    chore: generate sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -43,8 +43,8 @@ export default [
   {
     input: 'lib/index.js',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
     ],
     plugins: pgl()
   }
